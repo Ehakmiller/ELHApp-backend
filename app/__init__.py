12303cdc69bf6a0eb9a8ed73f627b460c868bca0
Ehakmiller <53654863+Ehakmiller@users.noreply.github.com>
2025-12-05 20:03:54 -0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
-echo "# ELHApp-backend" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ehakmiller/ELHApp-backend.git
-git push -u origin main
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Render/Supabase/Neon will give you DATABASE_URL as an environment variable.
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
+
+app = create_app()
