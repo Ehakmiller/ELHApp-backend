51ba3a7db3faa726e451f8839eb4f8518fe9092e
Ehakmiller <53654863+Ehakmiller@users.noreply.github.com>
2025-12-05 19:28:51 -0800

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+echo "# ELHApp-backend" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ehakmiller/ELHApp-backend.git
+git push -u origin main
